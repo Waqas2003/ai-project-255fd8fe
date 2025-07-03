9b74ceb86281672f5bd6fc98c4491a6a3a259a83
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-03 18:36:22 +0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='hello_world',
+    version='1.0',
+    packages=[''],
+    url='',
+    author='author',
+    author_email='',
+    description=''
+)
